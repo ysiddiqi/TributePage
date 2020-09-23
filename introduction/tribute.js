aa71e170b86a127ae250ac6a157c30ae5741cb44
ysiddiqi <yasmine.siddiqi@gmail.com>
2020-09-23 11:26:35 -0400

    first commit

---
+++ introduction/tribute.js
@@ -0,0 +1,17 @@
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+ //When the user scrolls down 20px from the top of the document, show the button
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+//When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
